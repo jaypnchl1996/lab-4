0e44726285053760f4e2449d505550dba3301455
jaypnchl1996 <159087814+jaypnchl1996@users.noreply.github.com>
2024-03-15 20:38:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello World! This is Jaykumar Panchal'  # Make sure this line is properly indented
+    return 'Hello World! This is Jaykumar Panchal. This file has been updated automatically by CI/CD'  # Make sure this line is properly indented
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
